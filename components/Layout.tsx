37af2af553ddf78bcc2d949970c44bfdd1a3d3e5
Diego Ramos <ramosdiego@pm.me>
2022-04-05 16:48:34 -0400

    increase padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 
 const Layout: FC = ({ children }) => (
-  <main className="mx-auto px-3 max-w-screen-2xl">{children}</main>
+  <main className="mx-auto px-6 max-w-screen-2xl">{children}</main>
 )
 
 export default Layout
