cc1beaf8b7352f8a8e836509bcedbdfb32d882c2
Diego Ramos <ramosdiego@pm.me>
2022-04-06 10:56:45 -0400

    added buy token

--- components/Buy.tsx
+++ components/Buy.tsx
@@ -1,12 +1,103 @@
-import React, { FC } from 'react'
+import { buyToken, Execute } from '@reservoir0x/client-sdk'
+import useCollection from 'hooks/useCollection'
+import * as Dialog from '@radix-ui/react-dialog'
+import React, { FC, useState } from 'react'
+import { useSigner } from 'wagmi'
+import { CgSpinner } from 'react-icons/cg'
+import ModalCard from './ModalCard'
+
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE
 
 const Buy: FC = () => {
+  const [{ data: signer }] = useSigner()
+  const [steps, setSteps] = useState<Execute['steps']>()
+  const [waitingTx, setWaitingTx] = useState<boolean>(false)
+  const [error, setError] = useState<any>(undefined)
+
+  const collection = useCollection()
+
+  const [open, setOpen] = useState(false)
+
+  const tokenId = collection.data?.collection?.floorAsk?.token?.tokenId
+  const token = `${CONTRACT_ADDRESS}:${tokenId}`
+
+  const close = () => {
+    // Close modal
+    setOpen(false)
+    // Reset steps
+    setSteps(undefined)
+    // Toggle off waiting state
+    setWaitingTx(false)
+  }
+
+  const handleSuccess: Parameters<typeof buyToken>[0]['handleSuccess'] = () => {
+    collection && collection.mutate()
+    setError(undefined)
+  }
+
+  const handleError: Parameters<typeof buyToken>[0]['handleError'] = (
+    err: any
+  ) => {
+    close()
+
+    setError(
+      <p className="text-[#FF3B3B] reservoir-body mb-4">
+        Insufficent funds.{' '}
+        <a
+          href="https://faucet.paradigm.xyz/"
+          rel="noopener noreferrer nofollow"
+          className="underline"
+        >
+          Top up your Rinkeby ETH
+        </a>{' '}
+        and try again.
+      </p>
+    )
+  }
+
+  const execute = async () => {
+    setWaitingTx(true)
+
+    await buyToken({
+      token,
+      signer,
+      apiBase: API_BASE,
+      setState: setSteps,
+      handleSuccess,
+      handleError,
+    })
+
+    setWaitingTx(false)
+  }
+
   return (
     <article className="mb-28">
       <div className="reservoir-h6 mb-11">Buy MultiFaucet NFT</div>
-      <button className="btn-primary-fill" disabled>
-        Buy Now
-      </button>
+      {error}
+      <Dialog.Root open={open} onOpenChange={setOpen}>
+        <Dialog.Trigger
+          disabled={waitingTx}
+          onClick={execute}
+          className="btn-primary-fill"
+        >
+          {waitingTx ? (
+            <CgSpinner className="h-4 w-4 animate-spin" />
+          ) : (
+            'Buy Now'
+          )}
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay>
+            <ModalCard
+              loading={waitingTx}
+              title="Buy Now"
+              close={close}
+              steps={steps}
+            />
+          </Dialog.Overlay>
+        </Dialog.Portal>
+      </Dialog.Root>
     </article>
   )
 }
---
+++ hooks/useCollection.ts
@@ -0,0 +1,30 @@
+import { paths, setParams } from '@reservoir0x/client-sdk'
+import fetcher from 'lib/fetcher'
+import useSWR from 'swr'
+
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE
+
+type Orders = paths['/collection/v1']['get']['responses']['200']['schema']
+
+export default function useCollection() {
+  function getUrl() {
+    if (!API_BASE || !CONTRACT_ADDRESS) return undefined
+
+    const url = new URL('/collection/v1', API_BASE)
+
+    let query: paths['/collection/v1']['get']['parameters']['query'] = {
+      id: CONTRACT_ADDRESS,
+    }
+
+    setParams(url, query)
+
+    return url.href
+  }
+
+  const href = getUrl()
+
+  const orders = useSWR<Orders>(href, fetcher)
+
+  return orders
+}
