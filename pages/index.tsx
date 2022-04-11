875ce9ad479468ec928485718d182e8bf087fbe0
Diego Ramos <ramosdiego@pm.me>
2022-04-11 15:28:31 -0400

    added links

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import List from 'components/List'
 import TokenOffer from 'components/TokenOffer'
 import useUserAsks from 'hooks/useUserAsks'
 import { useAccount } from 'wagmi'
+import { FiExternalLink } from 'react-icons/fi'
 
 const IndexPage = () => {
   // wagmi hooks
@@ -22,7 +23,24 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <h1 className="reservoir-h1 mb-20 mt-28">Reservoir Client SDK Demo</h1>
+      <h1 className="reservoir-h1 mb-2 mt-28">Reservoir Client SDK Demo</h1>
+      <a
+        className="reservoir-h5 inline-flex items-center mb-2"
+        href="https://github.com/reservoirprotocol/client-sdk-demo"
+        rel="norefereer noopener"
+      >
+        Source code
+        <FiExternalLink className="w-4 h-4 ml-2" />
+      </a>
+      <br />
+      <a
+        className="reservoir-h5 inline-flex items-center mb-14"
+        href="https://www.npmjs.com/package/@reservoir0x/client-sdk"
+        rel="norefereer noopener"
+      >
+        @reservoir0x/client-sdk
+        <FiExternalLink className="w-4 h-4 ml-2" />
+      </a>
       <ConnectWallet />
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-28">
         <Buy />
